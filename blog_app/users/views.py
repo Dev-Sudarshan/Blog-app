65d81076a6c4a2f1d1e17187043a31a6d9a05e1e
Dev-Sudarshan <sudarshanbhattarai04@gmail.com>
2025-01-31 22:31:55 +0545

    First Commit

---
+++ blog_app/users/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from users.forms import UserRegistrationForm
+
+
+
+
+def register(request):
+    if request.method == 'POST' :
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username= form.cleaned_data.get('username')
+            messages.success(request, f'{username} is registered')
+            return redirect('blog-home')
+    else:
+        form = UserRegistrationForm()
+    
+
+    return render(request, 'user/register.html', {'form': form})
