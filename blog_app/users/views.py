ef83b4071285c1bf277e002060b78cf487d53366
Dev-Sudarshan <sudarshanbhattarai04@gmail.com>
2025-02-04 12:03:41 +0545

    Second Commit

--- blog_app/users/views.py
+++ blog_app/users/views.py
@@ -1,20 +1,16 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from users.forms import UserRegistrationForm
-
-
+from .forms import UserRegisterForm
 
 
 def register(request):
-    if request.method == 'POST' :
-        form = UserRegistrationForm(request.POST)
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username= form.cleaned_data.get('username')
-            messages.success(request, f'{username} is registered')
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
             return redirect('blog-home')
     else:
-        form = UserRegistrationForm()
-    
-
-    return render(request, 'user/register.html', {'form': form})
+        form = UserRegisterForm()
+    return render(request, 'users/register.html' , {'form': form})
